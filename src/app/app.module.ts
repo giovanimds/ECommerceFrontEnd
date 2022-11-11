4fbf8b1c8c8303a8f5cba5193e775873b04d47d3
giovani <giovanimoresco@gmail.com>
2022-11-11 01:02:07 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrarProdutoComponent } from './components/pages/produto/cadastrar-produto/cadastrar-produto.component';
+import { CadastrarUsuarioComponent } from './components/pages/usuario/cadastrar-usuario/cadastrar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarProdutoComponent,
+    CadastrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
