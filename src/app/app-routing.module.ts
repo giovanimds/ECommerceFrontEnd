3bac4555ce3e48f33cdbdef99d988ff2808b97da
giovanimds <giovanimoresco@gmail.com>
2022-11-28 18:10:33 -0300

    navbar working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListarProdutoComponent} from "./components/pages/produto/listar-produto/listar-produto.component";
+import {BuscarUsuarioComponent} from "./buscar-usuario/buscar-usuario.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:"produtos", component: ListarProdutoComponent },
+  { path:"", component: ListarProdutoComponent },
+  { path:"conta", component: BuscarUsuarioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastrarProdutoComponent } from './components/pages/produto/cadastrar-produto/cadastrar-produto.component';
 import { CadastrarUsuarioComponent } from './components/pages/usuario/cadastrar-usuario/cadastrar-usuario.component';
 import { ListarProdutoComponent } from './components/pages/produto/listar-produto/listar-produto.component';
 import { ListarCarrinhoComponent } from './components/pages/carrinho/listar-carrinho/listar-carrinho.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { BuscarUsuarioComponent } from './buscar-usuario/buscar-usuario.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { ListarCarrinhoComponent } from './components/pages/carrinho/listar-carr
     CadastrarProdutoComponent,
     CadastrarUsuarioComponent,
     ListarProdutoComponent,
-    ListarCarrinhoComponent
+    ListarCarrinhoComponent,
+    BuscarUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
